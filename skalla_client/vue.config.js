bc60c686469200890a03ec569a13611de886bd65
kaggwachristopher <chrisduke.999@gmail.com>
2020-06-10 22:25:44 +0300

    changed production public path

--- skalla_client/vue.config.js
+++ skalla_client/vue.config.js
@@ -30,7 +30,5 @@ module.exports = {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
   },
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/'
-  : '/'
+  publicPath:'/'
 };
