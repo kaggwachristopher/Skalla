361e91df0791b0c630bf29d474f843cf03653e60
kaggwachristopher <chrisduke.999@gmail.com>
2020-07-12 14:04:33 +0300

    Production build

--- skalla_server/public/precache-manifest.531a554672a26cc58f2c9475fbdf07f2.js
+++ skalla_server/public/precache-manifest.531a554672a26cc58f2c9475fbdf07f2.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
-    "url": "/img/nucleo-icons.46abbc4a.svg"
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
   },
   {
     "revision": "431b4d35cf1594b9ecf40bb0305f13ef",
@@ -16,36 +16,40 @@ self.__precacheManifest = [
     "url": "/js/chunk-vendors.d093de3c.js"
   },
   {
-    "revision": "735ab4f94fbcd57074377afca324c813",
-    "url": "/robots.txt"
-  },
-  {
-    "revision": "e6d590e2c78bd87c2d32",
-    "url": "/js/demo.5b77fe2a.js"
-  },
-  {
     "revision": "59d7c2b119ab46fc7b5f853f04324c89",
     "url": "/img/brand/green1.png"
+  },
+  {
+    "revision": "6e4b6b7be14b080b7260",
+    "url": "/js/demo.fe733665.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
   },
   {
     "revision": "e0ea3f1916671872498288dc2bd40f06",
     "url": "/img/brand/white.png"
   },
   {
-    "revision": "2dabe9664922199995ac31c8fefd000a",
+    "revision": "37816b7be209f14fb0b8a7409ee780ad",
     "url": "/index.html"
   },
   {
-    "revision": "b6f7fdabeb5cc6a95c13",
-    "url": "/js/app.2caa6d6f.js"
+    "revision": "b4035ccedaf41aca08b5",
+    "url": "/js/app.cc36ffaa.js"
   },
   {
-    "revision": "431b4d35cf1594b9ecf40bb0305f13ef",
-    "url": "/img/logo.431b4d35.png"
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/img/nucleo-icons.46abbc4a.svg"
   },
   {
     "revision": "39f626707ed0846b6a57085c5b1e2027",
     "url": "/img/user.39f62670.png"
+  },
+  {
+    "revision": "431b4d35cf1594b9ecf40bb0305f13ef",
+    "url": "/img/logo.431b4d35.png"
   },
   {
     "revision": "a794a15f6e45b941d8a521e7965221a8",
@@ -56,10 +60,6 @@ self.__precacheManifest = [
     "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
   },
   {
-    "revision": "c1733565b32b585676302d4233c39da8",
-    "url": "/fonts/nucleo-icons.c1733565.eot"
-  },
-  {
     "revision": "426439788ec5ba772cdf94057f6f4659",
     "url": "/fonts/nucleo-icons.42643978.woff2"
   },
@@ -68,11 +68,11 @@ self.__precacheManifest = [
     "url": "/fonts/nucleo-icons.2569aaea.woff"
   },
   {
-    "revision": "e6d590e2c78bd87c2d32",
-    "url": "/css/demo.ccc4c7e5.css"
+    "revision": "6e4b6b7be14b080b7260",
+    "url": "/css/demo.0f56ee5e.css"
   },
   {
-    "revision": "b6f7fdabeb5cc6a95c13",
+    "revision": "b4035ccedaf41aca08b5",
     "url": "/css/app.6c53b5c5.css"
   }
 ];
--- skalla_server/public/service-worker.js
+++ skalla_server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c02766310afe5518b87f11ab3584d54.js"
+  "/precache-manifest.531a554672a26cc58f2c9475fbdf07f2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
