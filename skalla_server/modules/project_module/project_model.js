e5ccc38063c9d8e6dc36d1a42199764def6f1e29
kaggwachristopher <chrisduke.999@gmail.com>
2020-07-22 14:34:03 +0300

    Added consultant's estimate status field

--- skalla_server/modules/project_module/project_model.js
+++ skalla_server/modules/project_module/project_model.js
@@ -4,13 +4,14 @@ const Schema = mongoose.Schema
 
 //project schema and model
 const ProjectSchema = new Schema({
-    name: String,
+    name: {type:String, unique:true},
     developers:Number,
     dailyScrum: Number,
     pmsInvolved: Number,
     pmOverhead: Number,
     comments: String,
-    consultant: String
+    consultant: String,
+    consultantEstimate:{type:Boolean,default:false}
 })
 
 module.exports = mongoose.model('Project', ProjectSchema);
